6a75666a6d2a192912667af6eb30bfff79507d25
Joshorsley <146381020+Joshorsley@users.noreply.github.com>
2026-03-18 23:47:07 -0400

    add hero section with image

--- client/src/pages/landingpage.jsx
+++ client/src/pages/landingpage.jsx
@@ -1,7 +1,26 @@
+import { Link } from 'react-router-dom'
+import { Container, Button } from 'react-bootstrap'
+import heroBg from '../assets/heroSection.jpeg'
+
 function LandingPage() {
     return (
         <div>
-            <h1>Ithilien</h1>
+        <div className="hero-section text-center py-5"
+        style={{ backgroundImage: `url(${heroBg})` }}
+        >
+            <Container>
+            <div className='hero-content'>
+                <h1 className="hero-title">All things will grow with joy</h1>
+                <p className="hero-subtitle">
+                    Your personal plant care companion. Track watering schedules, get reminders
+                    and keep your plants thriving.
+                </p>
+                <Button as={Link} to="/register" variant="success" size="lg" className="mt-3">
+                    Get Started
+                </Button>
+            </div>
+            </Container>
+        </div>
         </div>
     )
 }

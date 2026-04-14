871029e99c0e052a44249010ad902c34ae9bb864
Mher Keshishian <Mkeshishian7933@conestogac.on.ca>
2026-04-14 14:46:51 -0400

    Add calendar header and weekdays row

--- client/src/pages/Calendar.jsx
+++ client/src/pages/Calendar.jsx
@@ -1,10 +1,19 @@
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 
 export default function Calendar() {
     return (
         <Container className='py-4'>
-            <h1>Watering Schedule</h1>
-            <p>Calendar page coming soon.</p>
+            <h1 className='mb-4'>Watering Schedule</h1>
+
+            <Row>
+                <Col>Sun</Col>
+                <Col>Mon</Col>
+                <Col>Tue</Col>
+                <Col>Wed</Col>
+                <Col>Thu</Col>
+                <Col>Fri</Col>
+                <Col>Sat</Col>
+            </Row>
         </Container>
     )
 }

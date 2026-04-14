ad390637ea9ecdb93cff722c5bbf20493a8036da
Mher Keshishian <Mkeshishian7933@conestogac.on.ca>
2026-04-14 14:45:24 -0400

    Create basic Calendar page placeholder

---
+++ client/src/pages/Calendar.jsx
@@ -0,0 +1,10 @@
+import { Container } from 'react-bootstrap'
+
+export default function Calendar() {
+    return (
+        <Container className='py-4'>
+            <h1>Watering Schedule</h1>
+            <p>Calendar page coming soon.</p>
+        </Container>
+    )
+}
